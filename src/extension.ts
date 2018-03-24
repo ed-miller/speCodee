498acb72b133c6b85562efb24cf5abafaf5eb3aa
Eduard S <ulean@student.kit.edu>
2018-03-24 17:48:27 +0100

    basic editing functionality

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,69 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import WebSocket = require('ws');
+
+
+function editText(pos:vscode.Position, text:string) {
+    vscode.window.activeTextEditor.edit(edit => {
+        edit.insert(/* vscode.window.activeTextEditor.selection.active */pos, text);
+        vscode.window.activeTextEditor.show();
+    }, {undoStopBefore:true, undoStopAfter:false });
+}
+
+function createClass(name:string) {
+    let doc = vscode.window.activeTextEditor.document;
+    const fullText = doc.getText()
+    let pos = doc.positionAt(fullText.length - 1);
+    editText(pos, "\r\nclass "+name+"\r\n"+"{\r\n\r\n\r\n\r\n\r\n\r\n}\r\n\r\n");
+}
+
+function createMethod(name:string) {
+    let doc = vscode.window.activeTextEditor.document;
+    const fullText = doc.getText()
+
+    let posClassEnd = fullText.lastIndexOf('}');
+    console.log('posClassEnd', posClassEnd);
+
+    if (posClassEnd > 0) { 
+        let pos = doc.positionAt(posClassEnd);
+        editText(pos, "\r\n\t"+name+"() {\r\n\t\t\r\n\t}\r\n\r\n");
+    }
+}
+
+function createAttribute(name:string, type:string) {
+    let doc = vscode.window.activeTextEditor.document;
+    const fullText = doc.getText();
+
+    let posClassBegin = fullText.indexOf('{');
+
+    if (posClassBegin > 0) { 
+        let pos = doc.positionAt(posClassBegin +1);
+        editText(pos,  "\r\n\tprivate "+name+":"+type+"\r\n");
+    }
+}
+
+function codeeGotoLine(line:number) {
+    const editor = vscode.window.activeTextEditor;
+    const position = editor.selection.active;
+
+    var newPosition = position.with(line > 0 ? line-1 : 0, 0);
+    var newSelection = new vscode.Selection(newPosition, newPosition);
+    editor.selection = newSelection;
+}
+
+function codeeMarkLine(line:number) {
+    const editor = vscode.window.activeTextEditor;
+    const position = editor.selection.active;
+
+    var newPosition = position.with(line > 0 ? line-1 : 0, 0);
+    var newPosition2 = position.with(line, 0);
+    var newSelection = new vscode.Selection(newPosition, newPosition2);
+    editor.selection = newSelection;
+}
+
+
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -18,12 +81,55 @@ export function activate(context: vscode.ExtensionContext) {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        vscode.window.showInformationMessage('Hello World 22!');
+        
+        // console.log('doc;', vscode.window.activeTextEditor.document.getText());
+        // createMethod('Farbwechsel');
+        createClass('Regenbogen');
+
     });
 
+    
+
+    disposable = vscode.commands.registerCommand('extension.createMethod', () => {
+        createMethod('Farbwechsel');
+    });
     context.subscriptions.push(disposable);
+
+    
+
+    disposable = vscode.commands.registerCommand('extension.codeeCreateAttribute2', () => {
+        createAttribute('Sättigung', 'number');
+    });
+    context.subscriptions.push(disposable);
+
+
+    
+
+    disposable = vscode.commands.registerCommand('extension.codeeGotoLine', () => {
+        codeeMarkLine(7);
+    });
+    context.subscriptions.push(disposable);
+
+
+/*     let wsc = new WebSocket();
+
+    const ws = new WebSocket('ws://locahost:5030/', "aaa");
+
+
+    wsc.on('open', function open() {
+        wsc.send('something');
+    });
+       
+    wsc.on('message', function incoming(data) {
+        console.log(data);
+    }); */
+
+    
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+
+
 }
